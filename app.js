36c95113235eb883b0aa6138e05f2d39ea850eae
Alexander Smith <alex.smith@focusedsupport.com>
2021-06-30 12:06:48 -0600

    fix public files for heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('public')
 })
 
 app.get('/test', async (req, res) => {
