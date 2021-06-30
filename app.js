c551fc3b3120c58b5b2e8f443efc363e85802e45
Alexander Smith <alex.smith@focusedsupport.com>
2021-06-30 12:02:07 -0600

    fix app.js for heroku

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require('express')
 // import express from 'express'
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 const square = require('./squareCLI.js')
 // import square from './squareCLI.js'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/test', async (req, res) => {
-    let result = await square.getOrders("9408KRRF2VWN2", 18)
-		for (const [key, value] of Object.entries(result)) {
-      result[key] = (parseFloat(value) / 100.00).toFixed(2).toString()
-    }
-    
-    res.send(result)
-})
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+app.get('/test', async (req, res) => {
+	let result = await square.getOrders("9408KRRF2VWN2", 18)
+	for (const [key, value] of Object.entries(result)) {
+		result[key] = (parseFloat(value) / 100.00).toFixed(2).toString()
+	}
+	
+	res.send(result)
+})
+
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+	console.log(`App is running on port ${port}`)
 })
